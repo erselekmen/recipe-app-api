ea5ac717558ec406cf7b743acffee6cb3b68c201
Ersel Ekmen <info@ratic.io>
2023-09-26 18:21:17 +0300

    Fix linting error: no blank space - 3

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,9 +2,9 @@ from app import calculator
 
 from django.test import SimpleTestCase
 
+
 class CalcTests(SimpleTestCase):
 
     def add_test(self):
         res = calculator.add(5, 6)
         self.assertEqual(res, 10)
-
